46d3373d33c2da50a85188c8a23829d7d39f3445
awefcxsd <m80102502@gmail.com>
2014-03-10 00:23:08 +0800

    bug video reconnect

--- ChatClient/src/ChatClient/video/trans/Recv.java
+++ ChatClient/src/ChatClient/video/trans/Recv.java
@@ -11,39 +11,40 @@ import javax.imageio.ImageIO;
 
 import ChatClient.util.ImageComponent;
 
-public class Recv implements Runnable{
+public class Recv implements Runnable {
 	DatagramSocket ds;
 	byte[] buffer;
 	ImageComponent recvImage;
-	public Recv(ImageComponent recv){
-		recvImage=recv;
+	public boolean action = true;
+
+	public Recv(ImageComponent recv) {
+		recvImage = recv;
 
 		// A byte array to read into (max size of 65536, could be smaller)
 		buffer = new byte[65536];
 	}
-	
-	
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		DatagramPacket p = new DatagramPacket(buffer, buffer.length);
-		
-		while (true) {
-			try {
+
+		try {
+			while (action) {
 				ds = new DatagramSocket(9100);
-				while (true){
-				ds.receive(p);
-				byte[] data = p.getData();
-				ByteArrayInputStream bais = new ByteArrayInputStream(data);
-				
-				// Make a BufferedImage out of the incoming bytes
-				BufferedImage bimg = ImageIO.read(bais);
-				recvImage.setImage(bimg);
-				
+				while (true) {
+					ds.receive(p);
+					byte[] data = p.getData();
+					ByteArrayInputStream bais = new ByteArrayInputStream(data);
+
+					// Make a BufferedImage out of the incoming bytes
+					BufferedImage bimg = ImageIO.read(bais);
+					recvImage.setImage(bimg);
+
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 
 		}
 	}
